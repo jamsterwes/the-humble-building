614084433e5f89904a7473974c70140c3b7f4242
Wes Taylor <jamsterwes@gmail.com>
2026-08-10 19:21:02 -0500

    refactor slider vec3

--- src/ui.rs
+++ src/ui.rs
@@ -1,50 +1,64 @@
 use cgmath::Euler;
 
 pub struct UiState {
-    pub xscale: f32,
-    pub yscale: f32,
-    pub zscale: f32,
-    pub xrot: f32,
-    pub yrot: f32,
-    pub zrot: f32,
+    pub scale: (f32, f32, f32),
+    pub rot: (f32, f32, f32),
     pub model_updated: bool,
 }
 
 impl UiState {
     pub fn new() -> Self {
         Self {
-            xscale: 1.0,
-            yscale: 1.0,
-            zscale: 1.0,
-            xrot: 0.0,
-            yrot: 0.0,
-            zrot: 0.0,
+            scale: (1.0, 1.0, 1.0),
+            rot: (0.0, 0.0, 0.0),
             model_updated: false,
         }
     }
 
     pub fn get_model_matrix(&self) -> cgmath::Matrix4<f32> {
         cgmath::Matrix4::from(Euler {
-            x: cgmath::Deg(self.xrot),
-            y: cgmath::Deg(self.yrot),
-            z: cgmath::Deg(self.zrot),
-        }) * cgmath::Matrix4::from_nonuniform_scale(self.xscale, self.yscale, self.zscale)
+            x: cgmath::Deg(self.rot.0),
+            y: cgmath::Deg(self.rot.1),
+            z: cgmath::Deg(self.rot.2),
+        }) * cgmath::Matrix4::from_nonuniform_scale(self.scale.0, self.scale.1, self.scale.2)
     }
+}
+
+// Returns true if a slider changed this frame
+pub fn slider_vec3_f32(
+    ui: &dear_imgui_rs::Ui,
+    id: &'static str,
+    value: &mut (f32, f32, f32),
+    min: (f32, f32, f32),
+    max: (f32, f32, f32),
+) -> bool {
+    let mut x = value.0;
+    let mut y = value.1;
+    let mut z = value.2;
+    let updated = ui.slider_f32(format!("X##{}", id), &mut x, min.0, max.0)
+        | ui.slider_f32(format!("Y##{}", id), &mut y, min.1, max.1)
+        | ui.slider_f32(format!("Z##{}", id), &mut z, min.2, max.2);
+    *value = (x, y, z);
+    return updated;
 }
 
 pub fn render_ui(_state: &mut UiState, ui: &dear_imgui_rs::Ui) {
     ui.window("Model Matrix").build(|| {
         ui.text("Scale:");
-        _state.model_updated = {
-            ui.slider_f32("X##scale", &mut _state.xscale, 0.1, 10.0)
-                | ui.slider_f32("Y##scale", &mut _state.yscale, 0.1, 10.0)
-                | ui.slider_f32("Z##scale", &mut _state.zscale, 0.1, 10.0)
-        };
+        _state.model_updated = slider_vec3_f32(
+            ui,
+            "scale",
+            &mut _state.scale,
+            (0.1, 0.1, 0.1),
+            (10.0, 10.0, 10.0),
+        );
         ui.text("Rotation (deg):");
-        _state.model_updated |= {
-            ui.slider_f32("X##rot", &mut _state.xrot, -180.0, 180.0)
-                | ui.slider_f32("Y##rot", &mut _state.yrot, -180.0, 180.0)
-                | ui.slider_f32("Z##rot", &mut _state.zrot, -180.0, 180.0)
-        };
+        _state.model_updated |= slider_vec3_f32(
+            ui,
+            "rot",
+            &mut _state.rot,
+            (-180.0, -180.0, -180.0),
+            (180.0, 180.0, 180.0),
+        );
     });
 }
